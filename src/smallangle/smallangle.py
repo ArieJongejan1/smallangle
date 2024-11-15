5482b10218dd1b5f2a2c750ab057f3fca4d9da33
ArieJongejan1 <a.jongejan1@chello.nl>
2024-11-15 16:36:47 +0100

    smallangle updates

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,20 +1,47 @@
 import click
 import numpy as np
-from numpy import pi
 import pandas as pd
+from numpy import pi
 
 
+@click.group()
+def cmd_group():
+    """
+    smallangle gives results of sinus and tangent of a number of function arguments
+    equally devide over the interval 0 to 2pi 
+    """
+    pass
+
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=5,
+    help = "provides a number of results"
+)
 def sin(number):
+    """ 
+    Provides sinus for a given value on the interval 0 to  2 pi. 
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=5,
+    help = "provides a given number of results"
+)
 def tan(number):
+    """ 
+    Provides tangent for a given value between 0 and 2 pi.
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
-
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
+
